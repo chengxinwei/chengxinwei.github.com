5b576a630570653d1e06b56eaefc610d967af535
chengxinwei <593040793@qq.com>
2013-06-09 15:46:09 +0800

    123

--- javascript/td/init.js
+++ javascript/td/init.js
@@ -55,65 +55,65 @@ var init = function(){
 
 var initImage = function(){
 	backGround_img = new Image();
-	backGround_img.src = 'image/background.png';
+	backGround_img.src = '/images/background.png';
 	
 	floor_img = new Image();
-	floor_img.src = 'image/floor.png';
+	floor_img.src = '/images/floor.png';
 	floor1_img = new Image();
-	floor1_img.src = 'image/floor1.png';
+	floor1_img.src = '/images/floor1.png';
 	floor2_img = new Image();
-	floor2_img.src = 'image/floor2.png';
+	floor2_img.src = '/images/floor2.png';
 	floor3_img = new Image();
-	floor3_img.src = 'image/floor3.png';
+	floor3_img.src = '/images/floor3.png';
 	floor4_img = new Image();
-	floor4_img.src = 'image/floor4.png';
+	floor4_img.src = '/images/floor4.png';
 	floor5_img = new Image();
-	floor5_img.src = 'image/floor5.png';	
+	floor5_img.src = '/images/floor5.png';	
 	icon_img = new Image();
-	icon_img.src = 'image/icon.png';
+	icon_img.src = '/images/icon.png';
 	icon_img1 = new Image();
-	icon_img1.src = 'image/icon1.png';
+	icon_img1.src = '/images/icon1.png';
 
 	logo_img = new Image();
-	logo_img.src = 'image/logo.png';
+	logo_img.src = '/images/logo.png';
 	back_img = new Image();
-	back_img.src = 'image/back.png';
+	back_img.src = '/images/back.png';
 	
 	tower1_img = new Image();
-	tower1_img.src = 'image/tower1.png';
+	tower1_img.src = '/images/tower1.png';
 	tower2_img = new Image();
-	tower2_img.src = 'image/tower2.png';
+	tower2_img.src = '/images/tower2.png';
 	tower3_img = new Image();
-	tower3_img.src = 'image/tower3.png';
+	tower3_img.src = '/images/tower3.png';
 	tower4_img = new Image();
-	tower4_img.src = 'image/tower4.png';
+	tower4_img.src = '/images/tower4.png';
 	mouster1_img = new Image();
-	mouster1_img.src= 'image/mouster1.png';
+	mouster1_img.src= '/images/mouster1.png';
 	mouster2_img = new Image();
-	mouster2_img.src= 'image/mouster2.png';
+	mouster2_img.src= '/images/mouster2.png';
 	mouster3_img = new Image();
-	mouster3_img.src= 'image/mouster3.png';
+	mouster3_img.src= '/images/mouster3.png';
 
 	mouster4_img = new Image();
-	mouster4_img.src= 'image/mouster4.png';
+	mouster4_img.src= '/images/mouster4.png';
 
 	end_img = new Image();
-	end_img.src = 'image/end.png';
+	end_img.src = '/images/end.png';
 	bullet1_img = new Image();
-	bullet1_img.src = 'image/bullet1.png';
+	bullet1_img.src = '/images/bullet1.png';
 	bullet2_img = new Image();
-	bullet2_img.src = 'image/bullet2.png';
+	bullet2_img.src = '/images/bullet2.png';
 	info_img = new Image();
-	info_img.src  =  'image/info.jpg';
+	info_img.src  =  '/images/info.jpg';
 
 	time_img = new Image();
-	time_img.src = 'image/time.png';
+	time_img.src = '/images/time.png';
 
 	blood_img = new Image();
-	blood_img.src = 'image/blood.gif';
+	blood_img.src = '/images/blood.gif';
 
 	slower_img = new Image();
-	slower_img.src = 'image/slower.png';
+	slower_img.src = '/images/slower.png';
 
 }
 
